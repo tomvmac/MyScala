b7d2b036d26b262fa6bbac0cca4c4c6080fb4fe4
Tom Mac <tmac@crunchtime.local>
2018-03-04 17:06:22 -0500

    Updated higher order functions with scanRight and scanLeft

--- MyScalaCookbook/src/functionalprogramming/HigherOrderFunctions.scala
+++ MyScalaCookbook/src/functionalprogramming/HigherOrderFunctions.scala
@@ -1,79 +1,81 @@
 package functionalprogramming
 
-
 /** Higher order functions replace loops and are useful because that can be passed into other functions and chained.   **/
 object HigherOrderFunctions extends App {
-  
- 
+
   PrintUtils.printHeader("Higher Order Function Examples")
-  
+
   val weekDays = List("Mon", "Tue", "Wed", "Thur", "Fri")
-  
+
   // Higher Order Functions that operate on one elment at a time
-  
+
   // Foreach
   PrintUtils.printHeader("Foreach Example")
   //weekDays.foreach(print(_))  // _ is the shorthand for the current item in the loop
   weekDays.foreach(x => print(x + " "))
-  
+
   // Map
   PrintUtils.printHeader("Map Example")
   //val mappedDays = weekDays.map(_ == "Mon") // short hand
   val mappedDays = weekDays.map(x => x == "Mon") // long version
-  
+
   // anonymous function
-  val isManicMonday = (x:String) => {
+  val isManicMonday = (x: String) => {
     x == "Mon": Boolean
   }
-  
+
   // Passing a function to a map function
   //val mappedDaysFunc = weekDays.map(isManicMonday)
-  
+
   // Passing a named function
   val mappedDaysFunc = weekDays.map(x => isThursday(x))
-  
+
   mappedDaysFunc.foreach(x => print(x + " "))
-   
-  
+
   // Filter
   PrintUtils.printHeader("Filter Examples")
   val filteredThurDays = weekDays.filter(x => isThursday(x))
   filteredThurDays.foreach(x => print(x + " "))
-    
+
   // Partition
   // Partition splits the orignial list into a two element tuple, a list of trues and a list of falses
   PrintUtils.printHeader("Partition Examples")
   val partitionedDays = weekDays.partition(x => isThursday(x))
   val partitionedTrues = partitionedDays._1
   val partitionedFalses = partitionedDays._2
- 
+
   partitionedTrues.foreach(x => print(x + " "))
   println(" ")
   partitionedFalses.foreach(x => print(x + " "))
-  
+
   // SortBy
   println("")
   weekDays.sortBy(_(0)).foreach(x => print(x + " "))
-  
-  
+
   /* -------------------------------------------------------------------------------------------------------------------------------------------- */
-  
+
   // Higher Order Functions that operate on multiple elments at a time
+
+  // Scan Right - right associative, return entire result list
+  PrintUtils.printHeader("Scan Right Example")
+  val someNumbers = List(10, 20, 30, 40, 50, 60)
+  val someNumbersScanRight = someNumbers.scanRight(0)(_ - _)
+  someNumbersScanRight.foreach(x => print(x + " "))
   
-  
-  // Scan Right
-   PrintUtils.printHeader("Scan Right Example")
-   
-  // Scan Left
-  
-  // Fold
-  
+  // Scan Left -left associative, return entire result list
+
+  // Fold -
+  // foldLeft - return only the last element after the last execution of scan left operation
+
   // Reduce
+  // reduceLeft - No initial value, first two list elments as operands in the first
+  // reduceRight
   
   
+
   def isThursday(x: String): Boolean = {
     x == "Thur": Boolean
-      
+
   }
-  
+
 }
