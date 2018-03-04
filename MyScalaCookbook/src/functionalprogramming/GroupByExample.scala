f113816a107780d66b7f47590feb02e136d9d41f
Tom Mac <tmac@crunchtime.local>
2018-03-04 10:24:11 -0500

    Updated GroupByExample with avg calc and map traverse

--- MyScalaCookbook/src/functionalprogramming/GroupByExample.scala
+++ MyScalaCookbook/src/functionalprogramming/GroupByExample.scala
@@ -1,49 +1,77 @@
 package functionalprogramming
 
 object GroupByExample extends App {
-  
+
   // Create a Car class
-  case class Car(val carMake: String, val carModel: String, val carType : String, price: Double, country: String)
+  case class Car(val carMake: String, val carModel: String, val carType: String, price: Double, country: String)
 
   var carList = List(Car("BMW", "330", "Sedan", 40000, "Germany"), Car("BMW", "500", "Sedan", 80000, "Germany"), Car("Porsche", "911", "Sports", 40000, "Germany"), Car("Ferrari", "460", "Sports", 400000, "Italy"), Car("Fiero", "Tesa", "Sports", 2000, "US"))
   var cheaperCarList = List(Car("Toyota", "Camry", "Sedan", 32000, "Japan"), Car("Hyundai", "Sonata", "Sedan", 28000, "Korea"), Car("Toyota", "Corolla", "Sedan", 28000, "Japan"))
   var usedCarList = List(Car("Toyota", "Camry", "Sedan", 10000, "Japan"), Car("Honda", "Civic", "Sedan", 14000, "Japan"), Car("Toyota", "Corolla", "Sedan", 8000, "Japan"))
-  
+  var minivanList = List(Car("Toyota", "Sienna", "Minivan", 40000, "Japan"), Car("Toyota", "Sienna", "Minivan", 45000, "Japan"), Car("Honda", "Odyessey", "Minivan", 43000, "Japan"))
+
   // merging list using union
-  carList = carList.union(cheaperCarList).union(usedCarList)
-   
+  carList = carList.union(cheaperCarList).union(usedCarList).union(minivanList)
+
   PrintUtils.printHeader("Initial List of cars")
   carList.foreach(println)
-  
+
   // GroupBy carType
   PrintUtils.printHeader("Cars grouped by carType")
   val carListGroupByType = carList.groupBy(_.carType)
   carListGroupByType.foreach(println)
-  
+
   // Transform List of List back to List of Cars
   PrintUtils.printHeader("List of Sports Cars")
   val sportsCars = carListGroupByType.get("Sports").toList.flatten
   sportsCars.foreach(println)
-  
+
   // Perform sortBy and get last element for highest price
   val highestPricedSportsCars = sportsCars.sortBy(x => x.price).last
   println("highest priced sports car is " + highestPricedSportsCars)
-  
-  val cheapestSportsCars = sportsCars.sortBy(x => - x.price).last
+
+  val cheapestSportsCars = sportsCars.sortBy(x => -x.price).last
   println("cheapestSportsCars priced sports car is " + cheapestSportsCars)
-  
+
   // Average Cost of a Japanese Sedan
   PrintUtils.printHeader("Average Cost of a Japanese Sedan")
   val japaneseSedans = carList.filter(_.carType == "Sedan").filter(_.country == "Japan")
   japaneseSedans.foreach(println)
-  
+
   println("")
-  
-  // Sum of prices in a given list of objects
-  val totalPriceOfJapaneseSedans = japaneseSedans.view.map(_.price).sum
-  println("totalPriceOfJapaneseSedans " + totalPriceOfJapaneseSedans)
-  
-  val avgPriceOfJapaneseSedans = totalPriceOfJapaneseSedans / japaneseSedans.length
-  println("avgPriceOfJapaneseSedans is " + avgPriceOfJapaneseSedans)
-  
+
+  println("avgPriceOfJapaneseSedans = " + avgPriceOfCars(japaneseSedans))
+
+  // Create a list of Avg price per cartype
+  // Start off with the map of carTypes, iterate though it, for each type, add the
+  // Create a function that calculates avg price based on list of cars
+  PrintUtils.printHeader("Average Cost of each car type")
+
+  val listBuilder = List.newBuilder[(String, Double)]
+
+  // loop through map
+  carListGroupByType.foreach(x => buildAvgCarList(x._1, x._2))
+  val carAvgList = listBuilder.result
+  carAvgList.foreach(println)
+
+  PrintUtils.printHeader("Car Type with highest Average Cost")
+  //  carAvgList.max(_2)
+  val highestAvgCarType = carAvgList.sortBy(_._2).last
+  println(highestAvgCarType)
+
+  /* --------------------------------------------------------------------------------------------------------------------------------------------------------*/
+
+  def buildAvgCarList(carType: String, carList: List[Car]) = {
+    var carAvgTuple = (carType, avgPriceOfCars(carList))
+    listBuilder += carAvgTuple
+
+  }
+
+  def avgPriceOfCars(carList: List[Car]): Double = {
+    // Sum of prices in a given list of objects
+    var sumPrice = carList.view.map(_.price).sum
+
+    sumPrice / carList.length
+  }
+
 }
--- MyScalaCookbook/src/functionalprogramming/TupleExample.scala
+++ MyScalaCookbook/src/functionalprogramming/TupleExample.scala
@@ -2,44 +2,45 @@ package functionalprogramming
 
 object TupleExample extends App {
 
-  var stat1= ("Kobe", "Bryant", "Lakers", 24, 81, 20, 11)
+  var stat1 = ("Kobe", "Bryant", "Lakers", 24, 81, 20, 11)
   var stat2 = ("Michael", "Jordan", "Bulls", 23, 60, 22, 11)
   var stat3 = ("Lebron", "James", "Cavaliers", 23, 50, 25, 18)
 
   // Iterating through a tuple, since tuple is not a collection
   println("Iterating through tuple values")
   stat1.productIterator.foreach(i => println("Value = " + i))
-  
-  
-  
+
   // Getting the length of the tuple
-  println("--------------------------------------------------------------------------------------------------------------")
+  PrintUtils.printHeader("Getting the length of the tuple")
   println("Getting the length of the tuple")
   println(stat1.productArity)
-  
-  
-  
+
   // Adding tuples to a list
-  println("--------------------------------------------------------------------------------------------------------------")
-  println("Adding tuples to a list")
-  val rawstats = List (stat1, stat2, stat3)
+  PrintUtils.printHeader("Adding tuples to a list")
+  val rawstats = List(stat1, stat2, stat3)
   rawstats.foreach(println)
-  
+
   //val highscore = rawstats.max(_._5)
-  println("--------------------------------------------------------------------------------------------------------------")
-  println("map function on a list of tuples")
+  PrintUtils.printHeader("map function on a list of tuples")
   val doubleScore = rawstats.map(_._5 * 2)
   doubleScore.foreach(println)
-  
-  
-  
- 
- /*------------------------------------------------------------------------------------------------------------*/
- 
- // two value tuple acts like a map
+
+  // Avg Tuple
+  PrintUtils.printHeader("Avg Tuple")
+  var carAvgTuple1 = ("Minivan", 40000.55)
+  var carAvgTuple2 = ("Sports", 2040000.77)
+  var carAvgTuple3 = ("Sedan", 20000.44)
+
+  val carAvgList = List(carAvgTuple1, carAvgTuple2, carAvgTuple3)
+  carAvgList.foreach(println)
+
+  val highestAvgCarType = carAvgList.sortBy(_._2).last
+  PrintUtils.printHeader("Highest Avg Car Type")
+  println(highestAvgCarType)
+
+  /*------------------------------------------------------------------------------------------------------------*/
+
+  // two value tuple acts like a map
   val genderPair = "Tom" -> "Male"
-  
-  
-  
-  
+
 }
